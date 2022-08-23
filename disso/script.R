48fba0f9047eace71d687cec7da03a282e0c241a
Cem <cemo.ustundag@gmail.com>
2022-08-24 02:23:02 +0300

    char to factor

--- disso/script.R
+++ disso/script.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 # load dataset as 'Data'
 Data = read.csv("Invistico_Airline.csv", stringsAsFactors = FALSE)
 
@@ -5,7 +7,6 @@ Data = read.csv("Invistico_Airline.csv", stringsAsFactors = FALSE)
 head(Data)
 summary(Data)
 dim(Data)
-is.na(Data)
 
 # inspect column types
 for(i in 1:(ncol(Data))) {
@@ -17,5 +18,33 @@ for(i in 1:(ncol(Data))) {
   }
   else{
     cat(sprintf("column %s is char\n", i))
+  }
+}
+
+# convert char columns to factor
+for(i in 1:ncol(Data)){
+  if(is.character(Data[, i]) == TRUE){
+    Data[, i] <- as.factor(Data[, i])
+    if(is.factor(Data[, i]) == TRUE){
+      cat(sprintf("feature %s has been converted into factor\n", i))
+      cat(sprintf("with levels: %s\n", levels(Data[, i])))
+    }
+  }
+}
+
+plot_list = list()
+for(i in 1:ncol(Data)){
+  var = names(Data)[i]
+  if(is.numeric(Data[, i]) == TRUE){
+    plot_list[[i]] = ggplot(Data, aes(, Data[, i])) + geom_boxplot() + labs(title = var)
+    # plot_list[[i]] = ggplot(Data, aes(, Data[, i])) + geom_histogram(bins = 10) + labs(title = var) + coord_flip()
+    print(plot_list[[i]])
+  }
+  else if(is.factor(Data[, i]) == TRUE){
+    plot_list[[i]] = ggplot(Data, aes(, Data[, i])) + geom_bar() + labs(title = var) + coord_flip()
+    print(plot_list[[i]])
+  }
+  else{
+    cat(sprintf("feature %s is not graphable\n", i))
   }
 }
